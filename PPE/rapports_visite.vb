d66604bdb85f167258d56ee2c99607b8104a26eb
Freizh <nathan.menou@gmail.com>
2020-12-11 08:37:57 +0100

    Premier commit

---
+++ PPE/rapports_visite.vb
@@ -0,0 +1,79 @@
+﻿Public Class rapports_visite
+
+
+
+  
+    'Text principal
+
+    Private Sub Label2_Click(sender As System.Object, e As System.EventArgs) Handles label_visite.Click
+
+    End Sub
+
+    
+
+
+    ' Text echanntillon données
+
+    Private Sub label_echantillon_Click(sender As System.Object, e As System.EventArgs) Handles label_echantillon.Click
+
+    End Sub
+
+
+
+    ' checkbox oui
+
+    Private Sub check_oui_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles check_oui.CheckedChanged
+
+    End Sub
+
+
+    ' checkbox non
+
+    Private Sub check_non_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles check_non.CheckedChanged
+
+    End Sub
+
+
+
+    ' Le texte nom du visiteur médical
+    Private Sub label_nom_visiteur_Click(sender As System.Object, e As System.EventArgs) Handles label_nom_visiteur.Click
+
+    End Sub
+
+
+
+    'Le nom et prénom du visiteur médical
+    Private Sub nom_visiteur_TextChanged(sender As System.Object, e As System.EventArgs) Handles nom_visiteur.TextChanged
+
+    End Sub
+
+
+    'Le texte prenom du visiteur médical
+
+    Private Sub label_prenom_visiteur_Click_1(sender As System.Object, e As System.EventArgs) Handles label_prenom_visiteur.Click
+
+    End Sub
+
+
+    ' le prénom du visiteur médical
+    Private Sub prenom_visiteur_TextChanged(sender As System.Object, e As System.EventArgs) Handles prenom_visiteur.TextChanged
+
+    End Sub
+
+
+
+    ' Le label informations sur le médecins
+    Private Sub label_info_Click(sender As System.Object, e As System.EventArgs) Handles label_info.Click
+
+    End Sub
+
+
+
+    ' Les informations sur le médecins
+    Private Sub info_medecin_TextChanged(sender As System.Object, e As System.EventArgs) Handles info_medecin.TextChanged
+
+    End Sub
+
+
+
+End Class
